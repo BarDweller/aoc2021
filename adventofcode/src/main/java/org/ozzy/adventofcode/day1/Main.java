a99048b827346ad680aafff987d2eee62017db26
Ozzy Osborne <bardweller@gmail.com>
2021-12-01 09:23:41 -0500

    Fix formatting, fix pom.xml..

--- adventofcode/src/main/java/org/ozzy/adventofcode/day1/Main.java
+++ adventofcode/src/main/java/org/ozzy/adventofcode/day1/Main.java
@@ -10,22 +10,22 @@ import one.util.streamex.StreamEx;
 
 public class Main {
 
-	public static void main(String[] args) throws Exception {
-		Path input = FileReader.getPathForClassPathResource("org/ozzy/adventofcode/day1/data");				
-		List<Integer> values = FileReader.getFileAsListOfInt(input);
-		
-		long part1 = IntStreamEx.of(values)
-				.pairMap((a,b)->(b>a ? b : 0))
-				.filter(a -> a>0)
-				.count();		
-		System.out.println(part1);
-		
-		long part2 = StreamEx.ofSubLists(values, 3, 1)
-				.map(a -> IntStreamEx.of(a).sum())
-				.pairMap((a,b)->(b>a ? b : 0))
-				.filter(a -> a>0)
-				.count();		
-		System.out.println(part2);
-	}
+  public static void main(String[] args) throws Exception {
+    Path input = FileReader.getPathForClassPathResource("org/ozzy/adventofcode/day1/data");
+    List<Integer> values = FileReader.getFileAsListOfInt(input);
+
+    long part1 = IntStreamEx.of(values)
+        .pairMap((a, b) -> (b > a ? b : 0))
+        .filter(a -> a > 0)
+        .count();
+    System.out.println(part1);
+
+    long part2 = StreamEx.ofSubLists(values, 3, 1)
+        .map(a -> IntStreamEx.of(a).sum())
+        .pairMap((a, b) -> (b > a ? b : 0))
+        .filter(a -> a > 0)
+        .count();
+    System.out.println(part2);
+  }
 
 }
