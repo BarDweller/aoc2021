f4b9691ae7b5ac96198b3ce337ea7ad07523d7a4
Ozzy Osborne <bardweller@gmail.com>
2021-12-02 11:24:55 -0500

    Add code for using fold rather than reduce

--- adventofcode/src/main/java/org/ozzy/adventofcode/day2/Main.java
+++ adventofcode/src/main/java/org/ozzy/adventofcode/day2/Main.java
@@ -36,8 +36,8 @@ public class Main {
     Delta result = 
         StreamEx.of(values)
             .map(a -> Delta.fromString(a))
-            .reduce(new Delta(0,0), (total, element) -> {return new Delta(total.x+element.x, 
-                                                                          total.y+element.y);});
+            .reduce(new Delta(0,0), (total, element) -> new Delta(total.x+element.x, 
+                                                                  total.y+element.y));
     System.out.println(result + " " +(result.x*result.y));
     
     //approach 1, map all deltas to part 2 deltas then reduce as per part1
@@ -45,9 +45,9 @@ public class Main {
         StreamEx.of(values)
             .map(a -> Delta.fromString(a))
             .map(a -> new PartTwoDelta(0, a.x, a.y))
-            .reduce(new PartTwoDelta(0,0,0), (total, element) -> {return new PartTwoDelta(total.aim+element.y, 
-                                                                                          total.x+element.x, 
-                                                                                          total.y+(total.aim*element.x));});
+            .reduce(new PartTwoDelta(0,0,0), (total, element) -> new PartTwoDelta(total.aim+element.y, 
+                                                                                  total.x+element.x, 
+                                                                                  total.y+(total.aim*element.x)));
     System.out.println(result2 + " " +(result2.x*result2.y));
     
     //approach2, use the reduce variant that allows for Type change, but requires a combining function for the new Type.
@@ -57,12 +57,38 @@ public class Main {
         StreamEx.of(values)
             .map(a -> Delta.fromString(a))
             .reduce(new PartTwoDelta(0,0,0), 
-                    (total, element) -> {return new PartTwoDelta(total.aim+element.y, 
-                                                                 total.x+element.x, 
-                                                                 total.y+(total.aim*element.x));},
+                    (total, element) -> new PartTwoDelta(total.aim+element.y, 
+                                                         total.x+element.x, 
+                                                         total.y+(total.aim*element.x)),
                     (total, othertotal) -> othertotal);
     
     System.out.println(result3 + " " +(result3.x*result3.y));
+    
+    
+    //using fold instead of reduce.. 
+    Delta foldresult1 = 
+        StreamEx.of(values)
+            .map(a -> Delta.fromString(a))
+            .foldLeft((a,b) -> new Delta(a.x+b.x, a.y+b.y))  //not providing a seed, means the result is optional (if the stream were empty)
+            .get();
+    System.out.println(foldresult1 + " " +(foldresult1.x*foldresult1.y));
+    
+    //provide initial seed to avoid that 'get' ;p
+    Delta foldresult1b = 
+        StreamEx.of(values)
+            .map(a -> Delta.fromString(a))
+            .foldLeft(new Delta(0,0),
+                      (a,b) -> new Delta(a.x+b.x, a.y+b.y));
+    System.out.println(foldresult1b + " " +(foldresult1b.x*foldresult1b.y));
+    
+    //part2 using fold feels cleaner than using reduce above =)
+    PartTwoDelta foldresult2 = 
+        StreamEx.of(values)
+            .map(a -> Delta.fromString(a))
+            .foldLeft(new PartTwoDelta(0,0,0),
+                      (PartTwoDelta a, Delta b) -> new PartTwoDelta(a.aim+b.y, a.x+b.x,a.y+(a.aim*b.x)));
+    System.out.println(foldresult2 + " " +(foldresult2.x*foldresult2.y));
+    
   }
 
 }
