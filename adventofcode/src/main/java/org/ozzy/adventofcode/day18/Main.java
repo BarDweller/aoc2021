8d44724ae88b132aba456d593e4b5874a9337b99
Ozzy Osborne <bardweller@gmail.com>
2021-12-18 16:21:16 -0500

    Missing commit

--- adventofcode/src/main/java/org/ozzy/adventofcode/day18/Main.java
+++ adventofcode/src/main/java/org/ozzy/adventofcode/day18/Main.java
@@ -159,7 +159,8 @@ public class Main {
     
     List<Pair> allNumbers = StreamEx.ofLines(input).map(Pair::new).toList();
     int max = StreamEx.ofCombinations(allNumbers.size(), 2)
-                      .map( i -> Math.max(allNumbers.get(i[0]).add(allNumbers.get(i[1])).magnitude(),allNumbers.get(i[1]).add(allNumbers.get(i[0])).magnitude()) )
+                      .map( i -> Math.max(allNumbers.get(i[0]).add(allNumbers.get(i[1])).magnitude(),
+                                          allNumbers.get(i[1]).add(allNumbers.get(i[0])).magnitude()) )
                       .max(Integer::compareTo)
                       .get();
     System.out.println(max);
