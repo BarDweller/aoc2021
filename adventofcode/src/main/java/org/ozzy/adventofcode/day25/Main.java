bacc00316df2be97d2c566ae1e83bde9601d455e
Ozzy Osborne <bardweller@gmail.com>
2021-12-26 12:26:30 -0500

    updated

--- adventofcode/src/main/java/org/ozzy/adventofcode/day25/Main.java
+++ adventofcode/src/main/java/org/ozzy/adventofcode/day25/Main.java
@@ -1,7 +1,6 @@
 package org.ozzy.adventofcode.day25;
 
 import java.nio.file.Path;
-import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Function;
 
@@ -14,50 +13,53 @@ import one.util.streamex.StreamEx;
 public class Main {
 
   record Coord(int x, int y) {};
-  record DirectionalMove(Function<Coord,Coord> movefn, Character target) {};
+  record AtCoord<T>(Coord coord, T obj) {};
 
-  private static Coord move(Map<Coord,Character> m, DirectionalMove d, Coord in) {
-    if(m.get(in) != d.target) return in;
-    Coord n = d.movefn.apply(in);
-    return m.containsKey(n) ? in : n;
+  private static Map<Coord,Character> move(Map<Coord,Character> map, char wanted, Function<Coord,Coord> newcoord) {
+    return EntryStream.of(map)
+                      .parallel()
+                      .flatMapKeyValue( (c,w) -> StreamEx.of(c)
+                                                         .map(newcoord)
+                                                         .map(nc -> !w.equals(wanted)||map.containsKey(nc)?new AtCoord<Character>(c,w):new AtCoord<Character>(nc,w))
+                                      )
+                      .toMap(c -> c.coord, c->c.obj);
   }
   
   public static void main(String[] args) throws Exception {
     Path input = FileReader.getPathForClassPathResource("org/ozzy/adventofcode/day25/data");
 
-    Map<Coord, Character> trench = new HashMap<>();
-    StreamEx.ofLines(input).zipWith(IntStreamEx.ints()).forKeyValue((line, lineNo) -> {
-      StreamEx.split(line, "").map(s -> s.charAt(0)).map(Character::valueOf).zipWith(IntStreamEx.ints())
-          .filterKeys(c -> c == '>' || c == 'v')
-          .forKeyValue((Character cucumber, Integer colNo) -> trench.put(new Coord(colNo, lineNo), cucumber));
-    });
-    
+    //all this, just to read in a grid of chars, keep the wanted ones, and store them in a map by x,y coord
+    Map<Coord, Character> trench =  StreamEx.ofLines(input)
+                                            .zipWith(IntStreamEx.ints())
+                                            .flatMapKeyValue((String line, Integer lineNo) -> 
+                                                                            StreamEx.split(line, "")
+                                                                                    .map(s -> s.charAt(0))
+                                                                                    .map(Character::valueOf)
+                                                                                    .zipWith(IntStreamEx.ints())
+                                                                                    .filterKeys(c -> c == '>' || c == 'v')
+                                                                                    .mapKeyValue((Character cucumber, Integer colNo) -> new AtCoord<Character>(new Coord(colNo,lineNo),cucumber))
+                                                             )
+                                            .toMap(c -> c.coord, c->c.obj);
+
+    //because we didn't just use nested for loops, now we have to go back to find the extents.
     int width = StreamEx.ofLines(input).findFirst().get().length();
     int height = (int) StreamEx.ofLines(input).count();
     
+    //define fn's that give the 'next' position for a direction
     Function<Coord, Coord> newh = (c) -> new Coord((c.x + 1) % width, c.y);
     Function<Coord, Coord> newv = (c) -> new Coord(c.x, (c.y + 1) % height);
-    
-    DirectionalMove h = new DirectionalMove(newh, '>');
-    DirectionalMove v = new DirectionalMove(newv, 'v');
 
     int count=1;
     while(true) {
-      Map<Coord,Character> afterHoriz = EntryStream.of(trench)
-                                                   .mapKeys( c -> move(trench,h,c))
-                                                   .toMap();
-      Map<Coord,Character> afterVert = EntryStream.of(afterHoriz)
-                                                  .mapKeys( c -> move(afterHoriz,v,c))
-                                                  .toMap();
-            
-      if(trench.equals(afterVert)) {
+      Map<Coord,Character> after = move(move(trench,'>',newh),'v',newv);
+      if(trench.equals(after)) {
         System.out.println(count);
         break;
       }
-      trench.clear();
-      trench.putAll(afterVert);
+      trench=after;
       count++;
     }
+    
   }
 
 }
